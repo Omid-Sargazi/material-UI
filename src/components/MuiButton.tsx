a3bf4f175e1670580cb4d645fe76858bff9909c5
OmidSargazi <om.sargazi@gmail.com>
2024-04-30 00:43:54 +0330

    Button Group in mui

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -5,15 +5,18 @@ import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import InfoIcon from '@mui/icons-material/Info';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import ButtonGroup from '@mui/material/ButtonGroup';
 export const MuiButton = ()=>{
     return (
    <Stack spacing={4}>
+
 < Stack spacing={2} direction="row">
     <h1>Button MUI</h1>
     <Button variant="text">Text</Button>
     <Button variant="contained" href="https://google.com">Contained</Button>
     <Button variant="outlined">Outlined</Button>
     </Stack>
+
     <Stack spacing={2} direction="row">
         <Button variant="contained" color="primary">Primary</Button>
         <Button disabled >Disable</Button>
@@ -28,10 +31,19 @@ export const MuiButton = ()=>{
         <Button variant="outlined" startIcon={<SendIcon/>}>Send Email</Button>
         <Button variant="contained" color="warning" startIcon={<CloudUploadIcon/>}>CloudUploadIcon</Button>
     </Stack>
+
     <Stack display="block" spacing={2} direction="row">
         <Button size="large" variant="outlined" color="warning" startIcon={<ErrorOutlineIcon/>}>Warning</Button>
         <Button size="medium" variant="contained" color="error" startIcon={< InfoIcon/>}>Medium</Button>
         <Button size="small" variant="contained" color="warning">Small</Button>
+    </Stack>
+
+    <Stack spacing={2} direction="row">
+       <ButtonGroup size="small" color="secondary" variant="text" orientation="vertical" aria-label="Basic button group">
+       <Button>Left</Button>
+        <Button>Center</Button>
+        <Button>Right</Button>
+       </ButtonGroup>
     </Stack>
    </Stack>
     )
